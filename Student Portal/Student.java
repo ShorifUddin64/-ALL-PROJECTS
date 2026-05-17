96ef89a188c3bd9848c535442ddefa11c95868f3
MD. SHORIF UDDIN <shorifuddin64662@gmail.com>
2026-05-18 03:20:16 +0600

    Update Student.java

--- Student Portal/Student.java
+++ Student Portal/Student.java
@@ -4,13 +4,9 @@ public class Student {
     private String course;
     private String section;
     private int credit;
-
-
-
     // ==========================
     // Constructor
     // ==========================
-
     public Student(
             String name,
             String course,
@@ -23,64 +19,39 @@ public class Student {
         this.credit = credit;
         this.section = section;
     }
-
-
-
-
     // ==========================
     // Setter Methods
     // ==========================
-
     public void setName(String p) {
-
         this.name = p;
     }
-
-
     public void setCourse(String c) {
-
         this.course = c;
     }
 
-
     public void setCredit(int cr) {
-
         this.credit = cr;
     }
-
 
     public void setSection(String s) {
 
         this.section = s;
     }
 
-
-
-
     // ==========================
     // Getter Methods
     // ==========================
-
     public String getName() {
-
         return name;
     }
 
-
     public String getCourse() {
-
         return course;
     }
-
-
     public int getCredit() {
-
         return credit;
     }
-
-
     public String getSection() {
-
         return section;
     }
 } 
