f7372b3453ca1a17ed57b1761136be6d75485cb7
MD. SHORIF UDDIN <shorifuddin64662@gmail.com>
2026-06-03 10:21:17 +0600

    Update Calculator.java

--- CALCULATOR/Calculator.java
+++ CALCULATOR/Calculator.java
@@ -1,3 +1,14 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Arrays;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
 public class Calculator {
-    
+    JFrame frame = new JFrame("Calculator");
+
+    Calculator() {
+        frame.setvisible(true);
+        frame.setSize(360, 540);
+    }
 }
